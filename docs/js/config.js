f6f4ccff91752a07185ab6f3f26625270d25ca86
Robert M. Horton, PhD <cybertory@gmail.com>
2026-06-12 22:39:12 -0700

    Add files via upload

--- docs/js/config.js
+++ docs/js/config.js
@@ -27,7 +27,7 @@ const SONGS = [
   {
     id: "all-about-me",
     title: "All About Me",
-    audio: "All_About_Me_v7.mp3",
+    audio: "All_About_Me.mp3",
     lyrics: "all_about_me.txt",
     description: "Origin story of the King"
   },
