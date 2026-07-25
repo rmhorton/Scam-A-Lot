6c6d681ff09c52f6c112872ef1b757de0488be15
Robert M. Horton, PhD <cybertory@gmail.com>
2026-07-25 00:59:26 -0700

    Add files via upload

---
+++ docs/configs/royal_hour.js
@@ -0,0 +1,17 @@
+const SONGS = [
+
+  {
+    id: "traffic_report",
+    title: "Traffic Report",
+    audio: "traffic_report.mp3",
+    lyrics: "traffic_report_script.txt",
+    pauseBeforeSeconds: 0,
+    description: "Don't get caught in traffic"
+  }
+  
+
+];
+
+// Paths relative to the docs/ folder
+const AUDIO_BASE = "../audio/";
+const LYRICS_BASE = "../lyrics/";
